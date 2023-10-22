3982d3324a046746319c7eda3259a648518f0b7a
EzhkaPro <gala-slim@yandex.ru>
2023-10-22 20:48:51 +0300

    4.2.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Здесь скоро будет Skypro.Music</header>
+      <header className="App-header"> Здесь скоро будет Skypro.Music</header>
     </div>
   );
 }
